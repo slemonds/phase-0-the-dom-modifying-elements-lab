69ab565084c4d34085a057845d0e86e2a9a4bf1d
Soyoung Lemonds <soyoung.lemonds@yahoo.com>
2023-05-26 21:07:30 -0400

    Adds index.js codes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+console.log(main);
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Soyoung is the champion";
